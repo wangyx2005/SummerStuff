fd208fcf51791b7ea924a5f2daff5f77c702f67d
YX Wang <wangyx2005@gmail.com>
2016-07-06 17:46:06 -0400

    generate runscript and dockerfile

--- pipeline/container_wrapper/container_wrapper.py
+++ pipeline/container_wrapper/container_wrapper.py
@@ -18,7 +18,7 @@ def show_dockerfile(system_name, container_name):
 def generate_runscript(input_path, output_path, name, command):
     with open('runscript_template', 'r') as myfile:
         script = myfile.read()
-    return script
+    return script % {'input': input_path, 'output': output_path, 'name': name, 'command': command}
 
 
 def show_runscript(input_path, output_path, name, command):
@@ -26,18 +26,39 @@ def show_runscript(input_path, output_path, name, command):
 
 
 def wrapper(alg_info):
+    '''
+    automatic generate dockerfile
+    para: a json object contains necessory information about algorithm
+    type: json
+    '''
+    # generate runscript
+    if alg_info['input_file_path'][-1] != '/':
+        alg_info['input_file_path'] += '/'
+
+    runscript = generate_runscript(alg_info['input_file_path'], alg_info[
+                                   'output_file_path'], alg_info['name'],
+                                   alg_info['run_command'])
+    with open('runscript.py', 'w') as tmpfile:
+        tmpfile.write(runscript)
+
     # generate dockerfile
     if alg_info['system'] not in SUPPORTED_SYSTEM:
         print("not support %s yet." % alg_info['system'])
         return
-    dockerfile = generate_dockerfile(alg_info['system'], alg_info['container_name'])
+    dockerfile = generate_dockerfile(
+        alg_info['system'], alg_info['container_name'])
     dockerfile_name = alg_info['name'] + '.Dockerfile'
     with open(dockerfile_name, 'w') as tmpfile:
         tmpfile.write(dockerfile)
 
-    if alg_info['input_file_path'][-1] != '/':
-        alg_info['input_file_path'] += '/'
+    return dockerfile_name
 
 
 if __name__ == '__main__':
-    if len(sys.argv) 
+    if len(sys.argv) < 2:
+        print('please input at least one file')
+
+    for file_name in sys.argv[1:]:
+        with open(file_name, 'r') as data_file:
+            alg = json.load(data_file)
+        wrapper(alg)
