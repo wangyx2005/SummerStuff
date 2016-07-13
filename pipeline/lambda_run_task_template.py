8fe5682ccdc3034062e35a46e8bc9e596477b52e
YX Wang <wangyx2005@gmail.com>
2016-07-13 11:34:10 -0400

    update setting alarm, cloudwatch alarm working

--- pipeline/lambda_run_task_template.py
+++ pipeline/lambda_run_task_template.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import boto3
 import json
+from time import time
 
 print('Loading lambda function')
 
@@ -11,23 +12,29 @@ cw = boto3.client('cloudwatch')
 
 
 def lambda_handler(event, context):
+    start_time = time()
     print("Received event: " + json.dumps(event, indent=2))
 
     QueueUrl = '%(queue_url)s'
     sqs.send_message(QueueUrl=QueueUrl, MessageBody=json.dumps(event))
 
     # add ec2 machine into ecs default cluster
-    instances = ec2.create_instances(ImageId='%(image_id)s', MinCount=1, KeyName='%(key_pair)s',
-                                     SecurityGroups='%(security_group)s', InstanceType='%(instance_type)s',
-                                     SubnetId='%(subnet_id)s', IamInstanceProfile={'Name': '%(iam_name)s'})
+    instances = ec2.create_instances(ImageId='%(image_id)s', MinCount=1,
+                                     KeyName='%(key_pair)s',
+                                     SecurityGroups='%(security_group)s',
+                                     InstanceType='%(instance_type)s',
+                                     SubnetId='%(subnet_id)s',
+                                     IamInstanceProfile={'Name': '%(iam_name)s'})
 
     # TODO: registe instances for cloudwatch shutdown
     alarm_name = instances[0].id + '-shutdown'
-    alarm_act = []
+    alarm_act = ['arn:aws:swf:%(region)s:%(account_id)s:action/actions/AWS_EC2.InstanceId.Stop/1.0']
     dimension = [{"Name": "InstanceId", "Value": instances[0].id}]
-    cw.put_metric_alarm(AlarmName=alarm_name, AlarmActions=alarm_act, MetricName='CPUUtilization',
-                        Namespace='AWS/EC2', Statistic='Average',
-                        Dimensions=dimension, Period=300, Unit='Seconds', EvaluationPeriods=2, Threshold=1, ComparisonOperator='LessThanThreshold')
+    cw.put_metric_alarm(AlarmName=alarm_name, AlarmActions=alarm_act,
+                        MetricName='CPUUtilization', Namespace='AWS/EC2',
+                        Statistic='Average', Dimensions=dimension, Period=300,
+                        Unit='Seconds', EvaluationPeriods=2,
+                        Threshold=1, ComparisonOperator='LessThanThreshold')
 
     # send the cloudwatch name for cleanup
     sqs.send_message(QueueUrl='%(alarm_sqs)s', MessageBody=alarm_name)
@@ -38,4 +45,5 @@ def lambda_handler(event, context):
     # run ecs task
     ecs.run_task(taskDefinition='%(task_name)s', count=1)
 
+    print('run time %f' % (time() - start_time))
     return 'send messages to sqs and start ecs'
