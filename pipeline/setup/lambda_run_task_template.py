b04a3e81f094648533df514dc4c9c65fa52e9edb
YX Wang <wangyx2005@gmail.com>
2016-07-14 13:10:00 -0400

    change folder name

---
+++ pipeline/setup/lambda_run_task_template.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import boto3
+import json
+from time import time
+
+print('Loading lambda function')
+
+sqs = boto3.client('sqs')
+ecs = boto3.client('ecs')
+ec2 = boto3.resource('ec2')
+cw = boto3.client('cloudwatch')
+
+
+def lambda_handler(event, context):
+    start_time = time()
+    print("Received event: " + json.dumps(event, indent=2))
+
+    QueueUrl = '%(sqs)s'
+    sqs.send_message(QueueUrl=QueueUrl, MessageBody=json.dumps(event))
+
+    # add ec2 machine into ecs default cluster
+    instances = ec2.create_instances(ImageId='%(image_id)s', MinCount=1,
+                                     KeyName='%(key_pair)s',
+                                     SecurityGroups='%(security_group)s',
+                                     InstanceType='%(instance_type)s',
+                                     SubnetId='%(subnet_id)s',
+                                     IamInstanceProfile={'Name': '%(iam_name)s'})
+
+    # TODO: registe instances for cloudwatch shutdown
+    alarm_name = instances[0].id + '-shutdown'
+    alarm_act = ['arn:aws:swf:%(region)s:%(account_id)s:action/actions/AWS_EC2.InstanceId.Stop/1.0']
+    dimension = [{"Name": "InstanceId", "Value": instances[0].id}]
+    cw.put_metric_alarm(AlarmName=alarm_name, AlarmActions=alarm_act,
+                        MetricName='CPUUtilization', Namespace='AWS/EC2',
+                        Statistic='Average', Dimensions=dimension, Period=300,
+                        Unit='Seconds', EvaluationPeriods=2,
+                        Threshold=1, ComparisonOperator='LessThanThreshold')
+
+    # send the cloudwatch name for cleanup
+    sqs.send_message(QueueUrl='%(alarm_sqs)s', MessageBody=alarm_name)
+
+    # wait instance running before start task
+    instances[0].wait_until_running()
+
+    # run ecs task
+    ecs.run_task(taskDefinition='%(task_name)s', count=1)
+
+    print('run time {}'.format((time() - start_time)))
+    return 'send messages to sqs and start ecs'
---
+++ pipeline/setup/readme.md
@@ -0,0 +1,1 @@
+CloudWatchActionsEC2Access
