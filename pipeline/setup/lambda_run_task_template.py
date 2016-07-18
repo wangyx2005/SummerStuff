ddb0e6189c68d80370812471d50356c8d1e7a569
YX Wang <wangyx2005@gmail.com>
2016-07-18 16:26:46 -0400

    change run_task to start_task running on the exact container

--- pipeline/setup/lambda_run_task_template.py
+++ pipeline/setup/lambda_run_task_template.py
@@ -11,9 +11,23 @@ ec2 = boto3.resource('ec2')
 cw = boto3.client('cloudwatch')
 
 
+def find_container(cluster, instance_id):
+    i = 1
+    while True:
+        ins = ecs.list_container_instances(cluster=cluster)
+        ins = ecs.describe_container_instances(cluster=cluster, containerInstances=ins['containerInstanceArns'])
+        ins = ins['containerInstances']
+        for info in ins:
+            if info['ec2InstanceId'] == instance_id:
+                return info['containerInstanceArn']
+        # print('run {} times'.format(i))
+        # i += 1
+
+
+
 def lambda_handler(event, context):
     start_time = time()
-    print("Received event: " + json.dumps(event, indent=2))
+    # print("Received event: " + json.dumps(event, indent=2))
 
     QueueUrl = '%(sqs)s'
     sqs.send_message(QueueUrl=QueueUrl, MessageBody=json.dumps(event))
@@ -38,12 +52,17 @@ def lambda_handler(event, context):
 
     # send the cloudwatch name for cleanup
     sqs.send_message(QueueUrl='%(alarm_sqs)s', MessageBody=alarm_name)
+    print('run time {}'.format((time() - start_time)))
 
     # wait instance running before start task
     instances[0].wait_until_running()
+    print('wait uitil ec2 running. run time {}'.format((time() - start_time)))
 
     # run ecs task
-    ecs.run_task(taskDefinition='%(task_name)s', count=1)
+    arn = find_container('default', instances[0].id)
+    print('find container time run time {}'.format((time() - start_time)))
+
+    ecs.start_task(taskDefinition='%(task_name)s', containerInstances=[arn])
 
     print('run time {}'.format((time() - start_time)))
     return 'send messages to sqs and start ecs'
